b5a1f1a949d429d780b01836ff44c5e3bf03eb0f
boybizarre <106738064+boybizarre@users.noreply.github.com>
2023-09-20 23:35:55 +0100

    update current user data

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,5 +1,16 @@
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+const filterObj = (obj, ...allowedFields) => {
+  const newObj = {};
+
+  Object.keys(obj).forEach((el) => {
+    if (allowedFields.includes(el)) newObj[el] = obj[el];
+  });
+
+  return newObj;
+};
 
 exports.getAllUsers = catchAsync(async (req, res) => {
   const users = await User.find();
@@ -11,6 +22,34 @@ exports.getAllUsers = catchAsync(async (req, res) => {
     results: users.length,
     data: {
       users,
+    },
+  });
+});
+
+exports.updateMe = catchAsync(async (req, res, next) => {
+  // 1) create error if user POSTs password data
+  if (req.body.password || req.body.passwordConfirm) {
+    return next(
+      new AppError(
+        'This route is not for password update. Please use /update-my-password',
+        400,
+      ),
+    );
+  }
+
+  // 2) filtering the req.body because we do not want to update every field that must've been passed into the body e.g role set to admin or passwordResetToken
+  const filteredBody = filterObj(req.body, 'name', 'email');
+
+  // 3) update user document
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, filteredBody, {
+    new: true,
+    runValidators: true,
+  });
+
+  res.status(200).json({
+    status: 'success',
+    data: {
+      user: updatedUser,
     },
   });
 });
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -16,6 +16,8 @@ router.patch(
   authController.updatePassword,
 );
 
+router.patch('/update-me', authController.protect, userController.updateMe);
+
 router
   .route('/')
   .get(userController.getAllUsers)
