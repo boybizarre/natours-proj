e0dcdc930cf3b117cde3aacbdfc51b3082d51a6b
boybizarre <106738064+boybizarre@users.noreply.github.com>
2023-10-06 23:12:48 +0100

    implemented mapbox and currently implementing log in functionality

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -1,7 +1,7 @@
 const Tour = require('../models/tourModel');
 const catchAsync = require('../utils/catchAsync');
 
-exports.getOverview = catchAsync(async (req, res) => {
+exports.getOverview = catchAsync(async (req, res, next) => {
   // 1) get tour data from collection
   const tours = await Tour.find();
   // 2) build template
@@ -12,7 +12,7 @@ exports.getOverview = catchAsync(async (req, res) => {
   });
 });
 
-exports.getTour = catchAsync(async (req, res) => {
+exports.getTour = catchAsync(async (req, res, next) => {
   // 1) get the data for the requested tour (including reviews and guides)
   const tour = await Tour.findOne({ slug: req.params.slug }).populate({
     path: 'reviews',
@@ -21,8 +21,22 @@ exports.getTour = catchAsync(async (req, res) => {
 
   // 2) build template
   // 3) render template using data from step 1
-  res.status(200).render('tour', {
-    title: `${tour.name} Tour`,
-    tour,
+  res
+    .status(200)
+    .set(
+      'Content-Security-Policy',
+      'connect-src https://*.tiles.mapbox.com https://api.mapbox.com https://events.mapbox.com',
+    ) // for mapbox
+    .render('tour', {
+      title: `${tour.name} Tour`,
+      tour,
+    });
+});
+
+exports.getLoginForm = catchAsync(async (req, res, next) => {
+  // const user = await tour.findBy
+
+  res.status(200).render('login', {
+    title: 'Log into your account',
   });
 });
--- public/js/mapbox.js
+++ public/js/mapbox.js
@@ -1,6 +1,54 @@
 /* eslint-disable */
 console.log('Hello from the client side!');
 
+// receiving the data from the html dataset
 const locations = JSON.parse(document.getElementById('map').dataset.locations);
-
 console.log(locations);
+
+// mapbox api
+mapboxgl.accessToken =
+  'pk.eyJ1IjoiYm95Yml6YXJyZSIsImEiOiJjbG5mMmlxOG0wMzA4MmltYjdoenJ1MjdkIn0.4RAicrif89kZqkX6v-h9cA';
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/boybizarre/clnf3aiqi021201qn2wcgblly',
+  scrollZoom: false,
+  // center: [-118.113491, 34.111745], // starting position [lng, lat]
+  // zoom: 9, // starting zoom
+  // interactive: false,
+});
+
+const bounds = new mapboxgl.LngLatBounds();
+
+locations.forEach((location) => {
+  // create marker
+  const element = document.createElement('div');
+  element.className = 'marker'; // already created class in css
+
+  // add marker
+  new mapboxgl.Marker({
+    element,
+    anchor: 'bottom',
+  })
+    .setLngLat(location.coordinates)
+    .addTo(map);
+
+  // add popup
+  new mapboxgl.Popup({
+    offset: 150,
+  })
+    .setLngLat(location.coordinates)
+    .setHTML(`<p>Day ${location.day}: ${location.description}</p>`)
+    .addTo(map);
+
+  // extend map bounds to include current location
+  bounds.extend(location.coordinates);
+});
+
+map.fitBounds(bounds, {
+  padding: {
+    top: 200,
+    bottom: 200,
+    left: 100,
+    right: 100,
+  },
+});
