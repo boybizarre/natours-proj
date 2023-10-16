5289ec7bece66096fd772d41ff3897c5bc800509
boybizarre <106738064+boybizarre@users.noreply.github.com>
2023-10-16 23:54:10 +0100

    responding to SIGTERM signal, heroku config

--- server.js
+++ server.js
@@ -41,3 +41,10 @@ process.on('unhandledRejection', (err) => {
     process.exit(1);
   });
 });
+
+process.on('SIGTERM', () => {
+  console.log('👋🏽 SIGTERM received. Shutting down gracefully...');
+  server.close(() => {
+    console.log('💥 Process terminated.');
+  });
+});
